3b223a20efb797f9d2cf05f56f1817bc7e15aa5a
rgarro <rgarro@gmail.com>
2017-10-24 22:48:58 -0600

    [#152222405] logout js

--- www/facturasjs/il8.js
+++ www/facturasjs/il8.js
@@ -12,3 +12,24 @@ translate.add({Enter: 'Entrar'}, 'es');
 
 translate.add({Reset: 'Reset'}, 'en');
 translate.add({Reset: 'Restablecer'}, 'es');
+
+translate.add({Logout: 'Logout'}, 'en');
+translate.add({Logout: 'Salir'}, 'es');
+
+translate.add({Profile: 'Profile'}, 'en');
+translate.add({Profile: 'Perfil'}, 'es');
+
+translate.add({Invoices: 'Invoices'}, 'en');
+translate.add({Invoices: 'Facturas'}, 'es');
+
+translate.add({Clients: 'Clients'}, 'en');
+translate.add({Clients: 'Clientes'}, 'es');
+
+translate.add({Users: 'Users'}, 'en');
+translate.add({Users: 'Usuarios'}, 'es');
+
+translate.add({MyCompany: 'My Company'}, 'en');
+translate.add({MyCompany: 'Mi Compañia'}, 'es');
+
+translate.add({Reports: 'Reports'}, 'en');
+translate.add({Reports: 'Reportes'}, 'es');
--- www/facturasjs/login.js
+++ www/facturasjs/login.js
@@ -3,10 +3,28 @@ var Login = (function(){
   function Login(){
     this.parentVars();
     this.authUrl = this.baseUrl + "login/auth";
+    this.signoutUrl = this.baseUrl + "/login/signout";
   }
 
   Login.prototype = Object.create(CRFut.FacturasCR.prototype);
   Login.prototype.constructor = Login;
+
+  Login.prototype.signout = function(){
+    var token = Cookies.get('token');
+    $.ajax({
+      url:this.signoutUrl,
+      data:{token:token},
+      type:"GET",
+      dataType:"json",
+      success:(function(data){
+          this.alert_success("Login Out ...");
+          Cookies.remove('Session');
+          Cookies.remove('User');
+          Cookies.remove('token');
+          window.location = "index.html";
+      }).bind(this)
+    });
+  }
 
   Login.prototype.submitAuth = function(data){
     data.password = SHA1(data.password);
@@ -16,7 +34,7 @@ var Login = (function(){
       type:"GET",
       dataType:"json",
       success:(function(data){
-        this.checkTimedOut(data);        
+        this.checkTimedOut(data);
         if(data.invalid_form == 1){
           this.alert_warning(data.flash);
         }else{
