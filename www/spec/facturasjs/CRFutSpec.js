1e12bb1d211d15a6465d2b63b370afe0003e0931
rgarro <rgarro@gmail.com>
2017-11-03 17:19:52 -0600

    [#152562762] frontend spec enviroment

---
+++ www/spec/facturasjs/CRFutSpec.js
@@ -0,0 +1,11 @@
+describe("CRFut",function(){
+
+	it("should have Object namespace",function(){
+        expect(CRFut).toBeObject();
+    });
+
+    it("should be testable",function(){
+        expect(true).toBeTrue();
+    });
+
+});
---
+++ www/spec/facturasjs/facturascrSpec.js
@@ -0,0 +1,13 @@
+describe("facturascr",function(){
+
+  var facturascr;
+
+  beforeAll(function(){
+			facturascr = new CRFut.FacturasCR();
+	});
+
+  it("should have parentVars method",function(){
+    expect(facturascr.parentVars).toBeFunction();
+  });
+
+});
