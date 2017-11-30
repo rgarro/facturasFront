4b3bd331dda8fb62301aa0667df4ef7686e65336
rgarro <rgarro@gmail.com>
2017-11-29 18:19:28 -0600

    [#153001272] lista emails widgets update I

--- www/facturasjs/widgets/extraEmails.js
+++ www/facturasjs/widgets/extraEmails.js
@@ -2,8 +2,15 @@ var extraEmails = (function(){
 
   function extraEmails(){
     this.parentVars();
-    this.newInput = "#extraEmailNewInput";
+    //this.newInput = "#extraEmailNewTInput";
+    this.newInput = "extraEmailNewTInput";
     this.hiddenInput = "#extraEmailNewInput";
+    this.addBtn = "#extraEmailAddBtn";
+    this.delBtn = ".delexmail";
+    this.displayBox = "#extraEmailsBox";
+    this.emailBoxSrc = "<span class='badge badge-pill badge-warning'>{{email}} <i index='{{index}}' class='fa fa-trash delexmail' style='cursor:pointer;'></i></span>";
+    this.emailBoxTpl = Handlebars.compile(this.emailBoxSrc);
+    this.extraEmails = [];
   }
 
   extraEmails.prototype = Object.create(CRFut.FacturasCR.prototype);
@@ -27,6 +34,18 @@ var extraEmails = (function(){
 
   extraEmails.prototype.updateHidden = function(){
 
+  }
+
+  extraEmails.prototype.init = function(){
+    //add button listener ...
+    $(this.addBtn).on("click",(function(evt){
+console.log(document.getElementById(this.newInput).validity.valid);
+
+    }).bind(this));
+    //del button listener
+    $(document).on(this.delBtn,"click",(function(evt){
+console.log(evt);
+    }).bind(this));
   }
 
   return extraEmails;
