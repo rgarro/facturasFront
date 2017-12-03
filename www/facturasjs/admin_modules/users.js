5c5c5cca4c9d135e23a61efcb1a439af50abc3c8
rgarro <rgarro@gmail.com>
2017-12-03 17:39:51 -0600

    [#153347631] new user form update

--- www/facturasjs/admin_modules/users.js
+++ www/facturasjs/admin_modules/users.js
@@ -3,13 +3,26 @@ var Users = (function(){
   function Users(){
     this.parentVars();
     this.dataTableUrl = this.baseUrl + "users/datatable";
+    this.AccessLevelsUrl = this.baseUrl + "users/accesslevelsoptions";
+    this.AccessLevelOptionsInput = "#userAccessLevelIDInput";
   }
 
   Users.prototype = Object.create(CRFut.FacturasCR.prototype);
   Users.prototype.constructor = Users;
 
   Users.prototype.save = function(){
-    
+
+  }
+
+  Users.prototype.getSetAccessLevelOptions = function(){
+    $.ajax({
+      url:this.AccessLevelsUrl,
+      type:"GET",
+      dataType:"json",
+      success:(function(data){
+        console.log(data);
+      }).bind(this)
+    });
   }
 
   return Users;
