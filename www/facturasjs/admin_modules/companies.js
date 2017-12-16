b3d16a9d7ae48609d3df04201c0317bb5850e9f1
rgarro <rgarro@gmail.com>
2017-12-16 16:39:11 -0600

    [#152993405] companias con logo update V

--- www/facturasjs/admin_modules/companies.js
+++ www/facturasjs/admin_modules/companies.js
@@ -5,13 +5,14 @@ var Companies = (function(){
     this.dataTableUrl = this.baseUrl + "companies/datatable";
     this.table = {};
     this.saveUrl = this.baseUrl + "companies/save";
+    this.BaseLogoUrl = this.baseUrl + "files/cialogos/";
   }
 
   Companies.prototype = Object.create(CRFut.FacturasCR.prototype);
   Companies.prototype.constructor = Companies;
 
   Companies.prototype.save = function(data){
-  
+
   }
 
   return Companies;
