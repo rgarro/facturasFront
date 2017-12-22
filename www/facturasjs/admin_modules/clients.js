10c827f8870448712a6f0a9621f6ae0959be09db
rgarro <rgarro@gmail.com>
2017-12-21 21:53:48 -0600

    [#153867649] clients can be removed

--- www/facturasjs/admin_modules/clients.js
+++ www/facturasjs/admin_modules/clients.js
@@ -5,6 +5,7 @@ var Clients = (function(){
     this.dataTableUrl = this.baseUrl + "clients/datatable";
     this.saveUrl = this.baseUrl + "clients/save";
     this.table = {};
+    this.deleteUrl = this.baseUrl + "clients/delete";
   }
 
   Clients.prototype = Object.create(CRFut.FacturasCR.prototype);
@@ -31,7 +32,26 @@ var Clients = (function(){
   }
 
   Clients.prototype.buildEdit = function(data){
-    
+
+  }
+
+  Clients.prototype.delete = function(data){
+    $.ajax({
+      url:this.deleteUrl+"?token="+Cookies.get("token"),
+      data:data,
+      type:"post",
+      dataType:"json",
+      success:(function(data){
+      this.verifyTokenizedRequest(data);
+        if(data.is_success == 1){
+          this.alert_success(data.flash);
+          this.table.ajax.reload();
+        }else{
+            this.alert_error(data.flash);
+            this.modelErrors(data.error_list);
+        }
+      }).bind(this)
+    });
   }
 
   return Clients;
