51b6217820f5c6bbaa4831f22b29d18fd8e51f3a
rgarro <rgarro@gmail.com>
2017-12-30 13:19:15 -0600

    [#153951845] company banks update

--- www/facturasjs/admin_modules/bankAccounts.js
+++ www/facturasjs/admin_modules/bankAccounts.js
@@ -7,6 +7,8 @@ var bankAccounts = class bankAccounts {
     this.parent = new CRFut.FacturasCR();
     this.parent.parentVars();
     this.getCompanyAccountsUrl = this.parent.baseUrl + "companies/companyaccounts";
+    this.htmlSrc = "{{#each companyAccounts}}<tr><td>{{CompanyID}} {{#if selected}} {{/if}} {{CompanyName}}</td></tr>{{/each}}";
+    this.template = Handlebars.compile(this.htmlSrc);
   }
 
 }
